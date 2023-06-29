1b5e76a9f9e5613bb0bb2a172f7d4b256aba4ac2
qoqme <rovnayainna@mail.ru>
2023-06-29 22:09:08 +0300

    Initial commit

---
+++ meetings-service/src/main/java/com/df_meetingsservice/meetingsservice/MeetingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.df_meetingsservice.meetingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsServiceApplication.class, args);
+	}
+
+}
---
+++ meetings-service/src/test/java/com/df_meetingsservice/meetingsservice/MeetingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.df_meetingsservice.meetingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
